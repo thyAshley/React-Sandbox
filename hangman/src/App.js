7efcab74565866d009ae1d44329bcfd37e9caae8
Ashley <thy.ashley@gmail.com>
2020-08-16 16:36:34 +0800

    added base template for hangman game

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./App.css";
+import Figure from "./components/Figure";
+import Header from "./components/Header";
+import Word from "./components/Word";
+import WrongLetters from "./components/WrongLetter";
+import Popup from "./components/Popup";
+import Notification from "./components/Notification";
+
+const words = ["application", "programming", "interface", "wizard"];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
-  return <div className="App"></div>;
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+        <Popup />
+        <Notification />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- hangman/src/components/Header.js
+++ hangman/src/components/Header.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <Fragment>
+      <h1>Hangman</h1>
+      <p>Find the hidden word - Enter a letter.</p>
+    </Fragment>
+  );
 };
 
 export default Header;
