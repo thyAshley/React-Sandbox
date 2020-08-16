6882233afe10cc6795dac29d176d88b73ac3683e
Ashley <thy.ashley@gmail.com>
2020-08-16 20:02:55 +0800

    added typescript with create react app

---
+++ github-finder/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const name: string = "John Doe";
+    return (
+      <div className="App">
+        <h1>Hello {name}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
