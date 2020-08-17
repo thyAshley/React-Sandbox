c56d4a4e84f9378ccc3963048ef763831786164a
Ashley <thy.ashley@gmail.com>
2020-08-17 14:01:51 +0800

    added axios to get data from github

--- github-finder/src/App.tsx
+++ github-finder/src/App.tsx
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import "./App.css";
 import NavigationBar from "./components/Layout/NavigationBar";
 import Users from "./components/Users/Users";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    console.log(res.data);
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
         <NavigationBar />
         <div className="container">
-          <Users />
+          <Users users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
     );
--- github-finder/src/components/Users/Users.tsx
+++ github-finder/src/components/Users/Users.tsx
@@ -7,34 +7,23 @@ const userStyle = {
   gridGap: "1rem",
 };
 
-export class Users extends Component {
-  state = {
-    users: [
-      {
-        login: "mojombo",
-        id: 1,
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        login: "defunkt",
-        id: 2,
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt",
-      },
-      {
-        login: "pjhyett",
-        id: 3,
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett",
-      },
-    ],
-  };
+type ObjClass = {
+  id: number;
+  login: string;
+  avatar_url: string;
+  html_url: string;
+};
 
+interface IProps {
+  users: ObjClass[];
+  loading: boolean;
+}
+
+export class Users extends Component<IProps> {
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItem
             key={user.id}
             login={user.login}
