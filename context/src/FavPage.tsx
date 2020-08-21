1b1f95ba4929511ca257d4b9ecfe6d85999aaa13
Ashley <thy.ashley@gmail.com>
2020-08-21 21:19:32 +0800

    completed application with usecontext, lazy and suspense

--- context/src/FavPage.tsx
+++ context/src/FavPage.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { Suspense, useContext } from "react";
 
-const FavPage = () => {
-  return <div>nothing</div>;
+import { Store } from "./Store";
+import { toggleFavAction } from "./Actions";
+import { IEpisodeProps } from "./interface";
+
+const EpisodeList = React.lazy<any>(() => import("./EpisodeList"));
+
+const FavPage = (): JSX.Element => {
+  const { state, dispatch } = useContext(Store);
+
+  const props: IEpisodeProps = {
+    episodes: state.favourites,
+    toggleFavAction,
+    favourites: state.favourites,
+    store: { state, dispatch },
+  };
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="episode-layout">
+        <EpisodeList {...props} />
+      </div>
+    </Suspense>
+  );
 };
 
 export default FavPage;
