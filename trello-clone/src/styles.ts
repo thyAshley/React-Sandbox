504eff06f595024725e99594211501d1c7fc7082
Ashley <thy.ashley@gmail.com>
2020-09-02 08:09:29 +0800

    added styled-components

--- trello-clone/src/styles.ts
+++ trello-clone/src/styles.ts
@@ -9,3 +9,18 @@ export const AppContainer = styled.div`
   padding: 20px;
   width: 100%;
 `;
+
+export const ColumnContainer = styled.div`
+  background-color: #ebecf0;
+  width: 300px;
+  min-height: 40px;
+  margin-right: 20px;
+  border-radius: 3px;
+  padding: 8px 8px;
+  flex-grow: 0;
+`;
+
+export const ColumnTitle = styled.div`
+  padding: 6px 16px 12px;
+  font-weight: bold;
+`;
