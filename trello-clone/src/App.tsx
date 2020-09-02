f63dec5b8af47a006519a956cc937db0a1efb869
Ashley <thy.ashley@gmail.com>
2020-09-02 21:43:56 +0800

    added add item function

--- trello-clone/src/App.tsx
+++ trello-clone/src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+import Card from "./Card";
+import Column from "./Column";
+import AddNewItem from "./AddNewItem";
 import { AppContainer } from "./styles";
 
 function App() {
-  return <AppContainer>Columns will go here</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate App Scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learning Typescript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Using Static Typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
+  );
 }
 
 export default App;
---
+++ trello-clone/src/NewItemForm.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+import { NewItemFormContainer, NewItemButton, NewItemInput } from "./styles";
+
+type NewItemFormProps = {
+  onAdd(text: string): void;
+};
+
+const NewItemForm = ({ onAdd }: NewItemFormProps) => {
+  const [text, setText] = useState("");
+  return (
+    <NewItemFormContainer>
+      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
+    </NewItemFormContainer>
+  );
+};
+
+export default NewItemForm;
