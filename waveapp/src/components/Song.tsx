563ec2b0be95ef69d8f7f22dd8ce2cf91c9b6ff6
Ashley <thy.ashley@gmail.com>
2020-11-27 17:03:40 +0800

    added wave app

---
+++ waveapp/src/components/Song.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Song = () => {
+  return (
+    <div className="song">
+      <h1>Title</h1>
+      <h1>Song</h1>
+      <h1>Artist</h1>
+    </div>
+  )
+}
+
+export default Song
