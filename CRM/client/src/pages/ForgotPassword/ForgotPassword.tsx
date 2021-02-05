364a455d0e7abc4d2d3e8ad9004abbcf6dadedbd
Ashley <thy.ashley@gmail.com>
2021-02-05 16:03:23 +0800

    touched up css

--- CRM/client/src/pages/ForgotPassword/ForgotPassword.tsx
+++ CRM/client/src/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./ForgotPassword.scss";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState<string>("");
@@ -10,12 +13,14 @@ const ForgotPassword = () => {
 
   return (
     <div className="w-3/4 h-auto p-5 m-5 bg-white login md:w-1/2 lg:w-1/3 rounded-3xl">
-      <h1 className="text-4xl font-bold text-center text-green-400">Login</h1>
+      <h1 className="text-4xl font-bold text-center text-green-400">
+        Forgot Password
+      </h1>
       {/* Form element */}
       <form className="form" onSubmit={onSubmitHandler}>
         <div>
           <div className="relative">
-            <label htmlFor="email">Email Address</label>
+            <label htmlFor="email">Enter Email</label>
             <input
               placeholder="Email"
               type="email"
@@ -25,13 +30,15 @@ const ForgotPassword = () => {
             />
           </div>
 
-          <input type="submit" value="Login" />
+          <input type="submit" value="Reset Password" />
         </div>
       </form>
 
       <hr className="border-green-100" />
       {/* Forgot password link */}
-      <p className="m-2 text-green-500">Forgot Password?</p>
+      <Link to="/">
+        <p className="m-2 text-green-500">Log in</p>
+      </Link>
     </div>
   );
 };
